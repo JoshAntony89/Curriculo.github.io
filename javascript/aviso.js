da6d7f9ef951dc99d8c7c008b11330557d08c068
JoshAntony89 <147880790+JoshAntony89@users.noreply.github.com>
2023-10-15 19:24:15 -0300

    Add files via upload

---
+++ javascript/aviso.js
@@ -0,0 +1,14 @@
+//Aqui exibi um alerta na pagina
+alert("Digite os seus Dados para contato")
+
+//Aqui exibi o que é para a pessoa digitar no site
+let nome="Digite o seu nome no campo nome: "
+let celular="Digite o seu numero de celular no campo numero: "
+let email="Digite o seu e-mail, no campo e-mail: "
+let mensagem="Digite uma mensagem para o Candidato: "
+
+//exibi os alertas na pagina do site
+alert(nome);
+alert(celular);
+alert(email);
+alert(mensagem);
---
+++ javascript/dadospessoais.js
@@ -0,0 +1,18 @@
+//Alerta do Javascipt
+alert("Dados do Jose")
+
+//Aqui eu declaro minhas variáveis para mostrar no site
+let nomeCompleto="Jose Antonio de Oliveira"
+let idade= 34;
+let bool= true;
+var cidade="Guanambi";
+let grauEscolar="Ensino Medio Completo";
+let grauSuperior="Cursando Analise e Desenvolvimento de Sistemas";
+
+//Aqui aparece meus alertas no site, quando a pessoa clica neles
+alert("Meu nome é: " +nomeCompleto)
+alert("Minha idade é: " +idade)
+alert("Dados: " +bool)
+alert("Minha Cidade é: " +cidade)
+alert("Escolaridade: " +grauEscolar)
+alert("Ensino Superior: " +grauSuperior)
